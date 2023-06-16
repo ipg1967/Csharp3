acc02cb42e0466659529984853af1ca97c61f8b1
ipg <>
2023-06-16 22:32:27 +0300

    ini

---
+++ zad21/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+ int readint(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void checkquarter(int x, int y)
+{
+if(x > 0 && y > 0)
+{
+    System.Console.WriteLine("Точка находится в 1-й четверти");
+}
+else if(x < 0 && y > 0)
+{
+    System.Console.WriteLine("Точка находится во 2-й четверти");
+}
+else if(x < 0 && y < 0)
+{
+    System.Console.WriteLine("Точка находится во 3-й четверти");
+}
+else if(x > 0 && y < 0)
+{
+    System.Console.WriteLine("Точка находится во 4-й четверти");
+}
+else
+{
+    System.Console.WriteLine("Введены неправильные координаты");
+}
+}
+// основной код
+int[] coordinates = new int[2];
+
+// System.Console.Write("Введите координату Х: ");
+// coordinates[0] = Convert.ToInt32(Console.ReadLine());
+coordinates[0] = readint("введите координату Х  "); // вместо верхнего кода - функция
+
+// System.Console.Write("Введите координату Y: ");
+// coordinates[1] = Convert.ToInt32(Console.ReadLine());
+coordinates[1] = readint("ВВедите координату Y  ");
+
+checkquarter(coordinates[0], coordinates[1]);
+
+// if(coordinates[0] > 0 && coordinates[1] > 0)
+// {
+//     System.Console.WriteLine("Точка находится в 1-й четверти");
+// }
+// else if(coordinates[0] < 0 && coordinates[1] > 0)
+// {
+//     System.Console.WriteLine("Точка находится во 2-й четверти");
+// }
+// else if(coordinates[0] < 0 && coordinates[1] < 0)
+// {
+//     System.Console.WriteLine("Точка находится во 3-й четверти");
+// }
+// else if(coordinates[0] > 0 && coordinates[1] < 0)
+// {
+//     System.Console.WriteLine("Точка находится во 4-й четверти");
+// }
+// else
+// {
+//     System.Console.WriteLine("Введены неправильны координаты");
+// }
---
+++ zad23/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// int readint(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// void checkq(int x)
+// {
+// if(x == 1)
+// {
+//     System.Console.WriteLine("Координаты четверти: х > 0, y > 0");
+// }
+// else if(x == 2)
+// {
+//     System.Console.WriteLine("Координаты четверти: х > 0, y < 0");
+// }
+// else if(x ==3)
+// {
+//     System.Console.WriteLine("Координаты четверти: х < 0, y < 0");
+// }
+// else if(x == 4)
+// {
+//     System.Console.WriteLine("Координаты четверти: х > 0, y < 0");
+// }
+// else
+// {
+//     System.Console.WriteLine("нет такой четверти");
+// }
+// }
+
+// int q = readint("Введите номер четверти");
+// checkq(q);
+
+// Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+System.Console.Write("Введите номер четверти: ");
+int qw = Convert.ToInt32(Console.ReadLine());
+string[] message = { "x > 0, y > 0", "x < 0, y > 0", "x < 0, y < 0", "x > 0, y < 0" };
+
+if (qw > 0 && qw < 5)
+{
+    System.Console.WriteLine(message[qw - 1]);
+}
+else
+{
+    System.Console.WriteLine("Такой четверти не существует!");
+}
